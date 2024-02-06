b10c0bef381971c2ca5e736666a81a50c503d0c8
ahmed-test <116157035+themeddev@users.noreply.github.com>
2024-02-06 17:18:01 +0100

    Add some modification to Hero Section in Hero Page

--- src/screens/pages/Hero/HeroSection.jsx
+++ src/screens/pages/Hero/HeroSection.jsx
@@ -3,7 +3,7 @@ import { FaCircleRight } from "react-icons/fa6";
 
 const HeroSection = () => {
   return (
-    <div className="md:max-w-[95%] h-full bg-gray-50 flex items-center mx-auto">
+    <div className="md:max-w-[95%] h-full bg-gray-50 flex items-center mx-auto mb-12">
       <section
         className="w-full bg-cover bg-center py-32 md:rounded-lg"
         style={{
@@ -13,8 +13,8 @@ const HeroSection = () => {
         }}
       >
         <div className="container mx-auto text-left ml-[10%]  ">
-          <h1 className="text-5xl font-medium mb-6 text-headersBlue">Embrace a healthier lifestyle</h1>
-          <p className="text-xl mb-12 text-myBlue"> Uplifting Lives Through the Path of Wellness and Vitality.</p>
+          <h1 className="md:text-5xl text-3xl font-medium mb-6 text-headersBlue">Embrace a healthier lifestyle</h1>
+          <p className="md:text-xl text-l mb-12 text-myBlue"> Uplifting Lives Through the Path of Wellness and Vitality.</p>
           <div href="#" className="inline-flex items-center bg-myOrange font-Poppins font-semibold text-white py-4 px-12 rounded-lg hover:bg-orange-600 duration-100">
             <span className="mr-3">Get Started</span>
             <FaCircleRight />
